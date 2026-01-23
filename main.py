68d73667413d906306917fb2218c197c32c2728e
Nkweb12 <kn7851@mci4me.at>
2026-01-23 11:20:31 +0100

    main ändern

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import ui_device
 import ui_reservations
 import ui_users
+import ui_wartungen
 
 st.set_page_config(page_title="Case Study 1", layout="wide")
 
@@ -10,12 +11,14 @@ st.write("Grundgerüst: UI → Logik → Datenbank (JSON)")
 
 seite = st.sidebar.selectbox(
     "Navigation",
-    ["Devices", "Reservierungen", "Nutzer"]
+    ["Devices", "Reservierungen", "Nutzer", "Wartungen"]
 )
 
 if seite == "Devices":
     ui_device.render()
 elif seite == "Reservierungen":
     ui_reservations.render()
+elif seite == "Wartungen":
+    ui_wartungen.render()
 else:
     ui_users.render()
